dbb64abeffeb06d991240568df8a41746ae8784f
SHIO777 <shiotatsu1015@gmail.com>
2022-11-08 22:50:57 +0900

    [add] definition to Log.cpp

--- ArduCopter/Log.cpp
+++ ArduCopter/Log.cpp
@@ -605,6 +605,7 @@ void Copter::log_init(void)
 
 #else // LOGGING_ENABLED
 
+void Copter::Log_Write_Test() {}
 void Copter::Log_Write_Control_Tuning() {}
 void Copter::Log_Write_Attitude(void) {}
 void Copter::Log_Write_EKF_POS() {}
